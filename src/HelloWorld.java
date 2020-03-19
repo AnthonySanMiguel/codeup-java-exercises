32f4522f295bed7c32074130d4df589cf096b0da
Anthony San Miguel <asanmiguel1088@gmail.com>
2020-03-19 16:15:11 -0500

    updated java intro exercise following walkthrough

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,6 +1,91 @@
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.print("Hello ");
-        System.out.print("World!");
+        System.out.println("World!");
+
+        int myFavoriteNumber = 4;                   // Step 1
+        System.out.println("Step 1: " + myFavoriteNumber);
+
+        String myString = "Testing: 1, 2, 3...";    // Step 2
+        System.out.println("Step 2: " + myString);
+
+//        String myString2 = 'A';                   // Step 3
+//        System.out.print("Step 3: " + myString2); // Doesn't complete build; Type "Char" not a String type
+
+        String myString3 = "3.14159";               // Step 4
+        System.out.println("Step 4: " + myString3);
+
+//        long myNumber;                            // Step 5
+//        System.out.print("Step 5: " + myNumber);  // Doesn't complete build; myNumber not initialized.
+
+        long myNumber2 = (long) 3.14;               // Step 6
+        System.out.println("Step 6: " + myNumber2);
+
+        long myNumber3 = 123L;                      // Step 7
+        System.out.println("Step 7: " + myNumber3);
+
+        long myNumber4 = 123;                       // Step 8
+        System.out.println("Step 8: " + myNumber4);
+
+        float myNumber5 = 3.14f;             // Step 9
+        System.out.println("Step 9: " + myNumber5);
+
+        int x = 5;                                  // Step 10
+        System.out.println("Step 10 (Part 1): " + x++);
+        System.out.println("Step 10 (Part 1): " + x);
+
+        int y = 5;
+        System.out.println("Step 10 (Part 2): " + ++y);
+        System.out.println("Step 10 (Part 2): " + y);
+
+//        class TEST;                                // Step 11
+//        System.out.println("Step 11: " + TEST);    // Throws error; identifier expected.
+
+//        String theNumberThree = "three";            // Step 12
+//        Object o = theNumberThree;
+//        int three = (int) o; // Throws runtime error; Exception in thread "main" java.lang.ClassCastException: class java.lang.String cannot be cast to class java.lang.Integer (java.lang.String and java.lang.Integer are in module java.base of loader 'bootstrap') at HelloWorld.main(HelloWorld.java:46)
+//        System.out.println("Step 12 (Part 1): " + theNumberThree);
+
+//        int three2 = (int) "three";
+//        System.out.println("Step 12 (Part 2): " + three2); // Throws compile error; String cannot be converted to Int type
+
+        int Z = 4;                                  // Step 13 (Part 1)
+        Z = Z + 5;
+//        System.out.println("Step 13 (Part 1): " + z);
+
+//        z = 4; // Shorthand
+//        z += 5; // Shorthand
+//        System.out.println("I'm using 'x' now " + z + "\n\n");
+
+        int a = 3;                                  // Step 13 (Part 2)
+        int b = 4;
+        b = b * a;
+//        System.out.println("Step 13 (Part 2): " + b);
+
+//        a = 3;
+//        b = 4;
+//        b *= 4; // Shorthand
+//        System.out.println("The value of b is %d and x is %d\n\n", b, a);
+
+        int c = 10;                                 // Step 13 (Part 3)
+        int d = 2;
+        c = c / d;
+        d = d - c;
+        System.out.println("Step 13 (Part 3, I): " + c);
+        System.out.println("Step 13 (Part 3, II): " + d);
+
+//        c = 10; // Shorthand
+//        d = 2;
+//        c /= d;
+//        d -= x;
+//        System.out.println("The value of c is %d and x is %d\n\n", c, d);
+
+        byte myByte = 0;                          // Step 14
+        int i = 0;
+        while(i < 129){
+            myByte++;
+            i++;
+        }
+        System.out.println("Step 14: myByte = " + myByte + "\n");
     }
 }
