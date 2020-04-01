3fceaea481d2efa87a24ee4d242af0e6df15c821
Anthony San Miguel <asanmiguel1088@gmail.com>
2020-04-01 10:05:00 -0500

    updated java collections lec

--- src/Collections.java
+++ src/Collections.java
@@ -53,7 +53,6 @@ public class Collections {
         System.out.println(roasts);
 
         roasts.remove(roasts.lastIndexOf("medium")); // Will remove last medium instance from list
-
         // 7. Remove the element at index 2
         roasts.remove(2);
         System.out.println(roasts);
@@ -65,16 +64,47 @@ public class Collections {
         // 1. Create a HashMap called usernames that contains:
         //   a. first name
         //   b. Github username
+        HashMap<String, String> usernames = new HashMap<>();
+
+        usernames.put("Casey", "fridaynext");
+        usernames.put("Vivian", "viviancan");
+        usernames.put("Trant", "trantbatey");
+
+        System.out.println(usernames);
+
         // 2. re-initialize the HashMap using the .clear() method
+        usernames.clear();
+        System.out.println(usernames);
+
         // 3. use the .put() method to add "Casey" -> "fridaynext" back to the map
+        usernames.put("Casey", "fridaynext");
+        System.out.println(usernames);
+
         // 4. use the .putIfAbsent() method to add
         //   a. "Vivian" -> "viviancan"
         //   b. "Casey" -> "XpenguinX"
+        usernames.putIfAbsent("Vivian", "viviancan");
+        usernames.putIfAbsent("Casey", "XpenguinX");
+        System.out.println(usernames);
+
         // 5. What happend with the .putIfAbsent()? Did both items get added? Why/why not?
+        //.putIfAbsent() only checks if the key is absent, and only will add the element if the key is absent
+
         // 6. Use the .remove() method to remove "Casey"
+        usernames.remove("Casey");
+        System.out.println(usernames);
+
         // 7. Use the .replace() method to change Vivian's username to "viviancandev"
+        usernames.replace("Vivian", "viviancandev");
+        System.out.println(usernames);
+
         // 8. Use the .clear() method to clear the map
+        usernames.clear();
+        System.out.println(usernames);
+
         // 9. Use the .isEmpty() method to verify that it was cleared
+        result = usernames.isEmpty();
+        System.out.println(result);
 
     }
 }
